617016acd74c3d0f42e3f60966e50f3842514d62
suleeman <sulemansarfraz17@yahoo.com>
2025-01-15 20:53:30 +1100

    cards component is ready without responsiveness

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./styles/App.scss"
 import { Header } from "./component/Header";
-import {Footer}  from "./component/Footer";
 import { Tcard } from "./component/T-card";
-
+import { Footer }  from "./component/Footer";
 function App() {
   return (
     <div className="App">
--- my-app/src/component/T-card.js
+++ my-app/src/component/T-card.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import "../styles/T-card.scss";
+import cardsData from "../localData/cardsData.json"
 
 export const Tcard = () => {
+    
   return (
     <div className='tcard'>
-        <div className='tcard-sub'> 
-        <img src={require("../images/facebook.png")}/>
-         <h4>Root canal</h4>
-         <p>We provide best suggestion on root canal and help our customer to retain their lives </p>
-    </div>
+      <div className='tcard-sub'  > 
+        {  cardsData.map((card, i) => {
+                { console.log(card.img);}
+           return (
+           
+            <div className='tcard-individual' key={i}> 
+            <video className= "tcard-image"  autoPlay  muted>
+               <source src={require("../images/vedio.mp4")} type="video/mp4" />
+               </video>
+            
+     
+            <div className='tcard-details'> 
+             <h4 className='tcard-heading'>{card.diseaseName}</h4>
+             <p className='tcard-description'>{card.description} </p>
+             </div>
+       
+        </div>)
+    
+        })
+    
+    }
+
+</div>
     </div>
   )
 }
